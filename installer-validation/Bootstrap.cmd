17ae173fb0815c31fc8084b12146ab1e759424d7
Minu-Park <im@minu.kr>
2026-07-23 15:34:40 +0900

    chore: remove obsolete IFW validation harness

--- installer-validation/Bootstrap.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Validation bootstrap started at %DATE% %TIME%>C:\ValidationResults\bootstrap-started.txt
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File C:\ValidationHarness\Run-Scenario.ps1 -Config C:\ValidationConfig\scenario.json -Output C:\ValidationResults >C:\ValidationResults\bootstrap.log 2>&1
-echo %ERRORLEVEL%>C:\ValidationResults\bootstrap-exit-code.txt
